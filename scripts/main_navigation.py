393be0879428e00968ea6f5d2ee83f7c9a17ea0c
Chris Paliqaw <christophomos@gmail.com>
2022-02-27 10:27:25 +0000

    Use POI.serv instead of Empty.srv

--- scripts/main_navigation.py
+++ scripts/main_navigation.py
@@ -2,20 +2,20 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_srvs.srv import Empty, EmptyRequest, EmptyResponse
+from rb1_localization.srv import POI, POIRequest, POIResponse
 
 class GoToPoint():
 
     SERVICE_NAME = "go_to_point"
     
     def __init__(self):
-        self._service = rospy.Service(GoToPoint.SERVICE_NAME, Empty, self.__service_callback)
+        self._service = rospy.Service(GoToPoint.SERVICE_NAME, POI, self.__service_callback)
         rospy.loginfo(f"Started service {GoToPoint.SERVICE_NAME}")
         self._ctrl_c = False
         rospy.on_shutdown(self.__shutdownhook)
     
-    def __service_callback(self, request: EmptyRequest):
-        return EmptyResponse()
+    def __service_callback(self, request: POIRequest):
+        return POIResponse()
         
     def __shutdownhook(self):
         self.ctrl_c = True
